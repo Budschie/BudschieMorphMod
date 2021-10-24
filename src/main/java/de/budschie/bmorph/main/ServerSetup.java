1cf225a0d93069518795facf3a27ea4f5e85035a
Budschie <jul.kp06@gmail.com>
2021-10-24 15:36:11 +0200

    Fixed bug where there would be no morph commands after reloading server

--- src/main/java/de/budschie/bmorph/main/ServerSetup.java
+++ src/main/java/de/budschie/bmorph/main/ServerSetup.java
@@ -3,7 +3,9 @@ package de.budschie.bmorph.main;
 import de.budschie.bmorph.capabilities.blacklist.ConfigManager;
 import de.budschie.bmorph.commands.BlacklistCommand;
 import de.budschie.bmorph.commands.MorphCommand;
+import net.minecraft.command.impl.ReloadCommand;
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -17,21 +19,26 @@ public class ServerSetup
 		
 	@SubscribeEvent
 	public static void onServerStarting(final FMLServerStartingEvent event)
-	{
-		MorphCommand.registerCommands(event.getServer().getCommandManager().getDispatcher());
-		BlacklistCommand.registerCommand(event.getServer().getCommandManager().getDispatcher());
-		
-		System.out.println("Registered commands.");
-		
+	{		
 		server = event.getServer();
 		
 		ConfigManager.INSTANCE.loadAll();
 	}
 	
 	@SubscribeEvent
+	public static void onServerReloaded(RegisterCommandsEvent event)
+	{
+		MorphCommand.registerCommands(event.getDispatcher());
+		BlacklistCommand.registerCommand(event.getDispatcher());
+		
+		System.out.println("Registered commands.");
+	}
+	
+	@SubscribeEvent
 	public static void onServerStopping(final FMLServerStoppingEvent event)
 	{
 		ConfigManager.INSTANCE.serverShutdown();
+		BMorphMod.DYNAMIC_ABILITY_REGISTRY.unregisterAll();
 		server = null;
 	}
 }
