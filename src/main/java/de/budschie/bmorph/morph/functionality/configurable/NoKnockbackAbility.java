7e21de9fd26cbf484c3172124ebac66097908dca
Budschie <jul.kp06@gmail.com>
2021-11-07 19:22:16 +0100

    Added chance to NoKnockbackAbility

--- src/main/java/de/budschie/bmorph/morph/functionality/configurable/NoKnockbackAbility.java
+++ src/main/java/de/budschie/bmorph/morph/functionality/configurable/NoKnockbackAbility.java
@@ -1,20 +1,32 @@
 package de.budschie.bmorph.morph.functionality.configurable;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import de.budschie.bmorph.morph.functionality.AbstractEventAbility;
-import de.budschie.bmorph.morph.functionality.codec_addition.ModCodecs;
 import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class NoKnockbackAbility extends AbstractEventAbility
 {
-	public static final Codec<NoKnockbackAbility> CODEC = ModCodecs.newCodec(NoKnockbackAbility::new);
+	public static final Codec<NoKnockbackAbility> CODEC = RecordCodecBuilder.create(instance -> instance.group(Codec.FLOAT.optionalFieldOf("chance", 1.0f).forGetter(NoKnockbackAbility::getChance)).apply(instance, NoKnockbackAbility::new));
+	
+	private float chance;
+	
+	public NoKnockbackAbility(float chance)
+	{
+		this.chance = chance;
+	}
 	
 	@SubscribeEvent
 	public void onLivingKnockbackEvent(LivingKnockBackEvent event)
 	{
-		if(isTracked(event.getEntity()))
+		if(isTracked(event.getEntity()) && event.getEntityLiving().world.getRandom().nextFloat() < chance)
 				event.setCanceled(true);
+	}
+	
+	public float getChance()
+	{
+		return chance;
 	}
 }
