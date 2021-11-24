f46495c61a85f0cc8903e17c5ef892e6d20340b0
Budschie <jul.kp06@gmail.com>
2021-11-24 17:51:19 +0100

    Chicken Synchronizer added

---
+++ src/main/java/de/budschie/bmorph/render_handler/ChickenSynchronizer.java
@@ -0,0 +1,48 @@
+package de.budschie.bmorph.render_handler;
+
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+// The fact that I have to do this sh*t shows how horrible some parts of MC code really are...
+@EventBusSubscriber(bus = Bus.FORGE, value = Dist.CLIENT)
+public class ChickenSynchronizer
+{
+	@SubscribeEvent
+	public static void applyToMorphEntity(PlayerTickEvent event)
+	{
+		if(event.side == LogicalSide.CLIENT && event.phase == Phase.START)
+		{
+			Entity morphEntity = RenderHandler.getCachedEntity(event.player);
+			
+			if(morphEntity instanceof Chicken chicken)
+			{
+				boolean groundOrFlying = event.player.getAbilities().flying || chicken.isOnGround();
+				
+				// Assign old values
+				chicken.oFlapSpeed = chicken.flapSpeed;
+				chicken.oFlap = chicken.flap;
+		
+				// Change flying speed, accordingly to whether you are flying/on ground or not.
+				chicken.flapSpeed += (groundOrFlying ? -1.0f : 4.0f) * 0.3f;
+				chicken.flapSpeed = Mth.clamp(chicken.flapSpeed, 0.0f, 1.0f);
+				
+				if (!groundOrFlying && chicken.flapping < 1.0f)
+				{
+					chicken.flapping = 1.0f;
+				}
+		
+				chicken.flapping = chicken.flapping * 0.9f;
+		
+				chicken.flap += chicken.flapping * 2.0f;
+			}
+		}
+	}
+}
