8f112d8cf02f637ea2648d3ef5aefe812f3468d4
Budschie <jul.kp06@gmail.com>
2021-11-24 18:25:28 +0100

    Parrots now have flying animation too

--- src/main/java/de/budschie/bmorph/render_handler/FlyingSynchronizer.java
+++ src/main/java/de/budschie/bmorph/render_handler/FlyingSynchronizer.java
@@ -3,6 +3,7 @@ package de.budschie.bmorph.render_handler;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.animal.Parrot;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
@@ -13,7 +14,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 // The fact that I have to do this sh*t shows how horrible some parts of MC code really are...
 @EventBusSubscriber(bus = Bus.FORGE, value = Dist.CLIENT)
-public class ChickenSynchronizer
+public class FlyingSynchronizer
 {
 	@SubscribeEvent
 	public static void applyToMorphEntity(PlayerTickEvent event)
@@ -42,6 +43,10 @@ public class ChickenSynchronizer
 				chicken.flapping = chicken.flapping * 0.9f;
 		
 				chicken.flap += chicken.flapping * 2.0f;
+			}
+			else if(morphEntity instanceof Parrot parrot)
+			{
+				parrot.calculateFlapping();
 			}
 		}
 	}
