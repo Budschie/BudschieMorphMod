09a13068e3f2442d92070f26838cb42d886c4fb1
Budschie <jul.kp06@gmail.com>
2021-11-29 17:11:04 +0100

    NBT paths now have a codec

--- src/main/java/de/budschie/bmorph/json_integration/NBTPath.java
+++ src/main/java/de/budschie/bmorph/json_integration/NBTPath.java
@@ -1,11 +1,16 @@
 package de.budschie.bmorph.json_integration;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 
 public class NBTPath
 {
 	private String[] nodes;
+	
+	public static final Codec<NBTPath> CODEC = Codec.STRING.comapFlatMap(fromStr -> DataResult.success(NBTPath.valueOf(fromStr)), toStr -> toStr.toString());
 	
 	public static NBTPath valueOf(String path)
 	{
@@ -22,6 +27,7 @@ public class NBTPath
 		return nodes;
 	}
 	
+	/** Returns the tag that sits at the current nbt path. **/
 	public Tag resolve(CompoundTag compoundNBT)
 	{
 		CompoundTag currentCompound = compoundNBT;
@@ -34,6 +40,20 @@ public class NBTPath
 	
 	public void copyTo(CompoundTag from, CompoundTag to)
 	{
+		copyTo(from, to, this);
+	}
+	
+	/**
+	 * Copies the element sitting at this nbt path to the given compound tag and the
+	 * given toPath.
+	 * 
+	 * @param from   This is the tag from which we shall copy the data from.
+	 * @param to     This is the tag to which we shall copy the data to.
+	 * @param toPath This is the nbt path representing the desination to which we
+	 *               shall copy.
+	 **/
+	public void copyTo(CompoundTag from, CompoundTag to, NBTPath toPath)
+	{
 		Tag fromNBT = resolve(from);
 		
 		if(fromNBT == null)
@@ -41,14 +61,15 @@ public class NBTPath
 		
 		CompoundTag currentCompound = to;
 		
-		for(int i = 0; i < nodes.length - 1; i++)
+		// Resolve the path where where we shall copy all nbt data to.
+		for(int i = 0; i < toPath.nodes.length - 1; i++)
 		{
-			CompoundTag newCompound = currentCompound.getCompound(nodes[i]);
-			currentCompound.put(nodes[i], newCompound);
+			CompoundTag newCompound = currentCompound.getCompound(toPath.nodes[i]);
+			currentCompound.put(toPath.nodes[i], newCompound);
 			currentCompound = newCompound;
 		}
 		
-		currentCompound.put(nodes[nodes.length - 1], fromNBT.copy());
+		currentCompound.put(toPath.nodes[toPath.nodes.length - 1], fromNBT.copy());
 	}
 	
 	@Override
