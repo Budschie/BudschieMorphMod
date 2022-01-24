b1f6844fdd6d53db595387e5d032e5d12a457fb6
Budschie <jul.kp06@gmail.com>
2022-01-24 13:51:03 +0100

    Laying foundations for the creation of audiovisual effects

---
+++ src/main/java/de/budschie/bmorph/morph/functionality/codec_addition/AudioVisualEffect.java
@@ -0,0 +1,21 @@
+package de.budschie.bmorph.morph.functionality.codec_addition;
+
+import java.util.ArrayList;
+
+import de.budschie.bmorph.util.ParticleCloudInstance;
+import de.budschie.bmorph.util.SoundInstance;
+
+/**
+ * This is a class that represents a bunch of audio and visual effects that can
+ * be played when a morph uses its ability.
+ **/
+public class AudioVisualEffect
+{
+	private ArrayList<SoundInstance> sounds;
+	private ArrayList<ParticleCloudInstance> particleCloudInstance;
+	
+	public AudioVisualEffect()
+	{
+		
+	}
+}
