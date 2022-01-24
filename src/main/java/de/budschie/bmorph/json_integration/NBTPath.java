b7f8578b9a1180dca8a07d08df5fcc3b9f40295e
Budschie <jul.kp06@gmail.com>
2022-01-24 13:50:26 +0100

    Added getOrCreateParent method and getter for the last node name

--- src/main/java/de/budschie/bmorph/json_integration/NBTPath.java
+++ src/main/java/de/budschie/bmorph/json_integration/NBTPath.java
@@ -27,6 +27,40 @@ public class NBTPath
 		return nodes;
 	}
 	
+	/**
+	 * Returns the name of the last node (meaning the final object => the object
+	 * this path points to.)
+	 **/
+	public String getLastNode()
+	{
+		return nodes[nodes.length - 1];
+	}
+	
+	/** Gets or creates a parent tag for the object represented by the Nbt path. **/
+	public CompoundTag getOrCreateParent(CompoundTag compound)
+	{
+		CompoundTag currentCompound = compound;
+		
+		// Resolve the path where where we shall copy all nbt data to.
+		for(int i = 0; i < nodes.length - 1; i++)
+		{
+			boolean doesNodeExist = currentCompound.contains(nodes[i]);
+			CompoundTag newCompound = currentCompound.getCompound(nodes[i]);
+			
+			if(!doesNodeExist)
+				currentCompound.put(nodes[i], newCompound);
+			
+			currentCompound = newCompound;
+		}
+		
+		return currentCompound;
+	}
+	
+	public void setTag(CompoundTag root, Tag tag)
+	{
+		getOrCreateParent(root).put(getLastNode(), tag);
+	}
+	
 	/** Returns the tag that sits at the current nbt path. **/
 	public Tag resolve(CompoundTag compoundNBT)
 	{
@@ -59,17 +93,8 @@ public class NBTPath
 		if(fromNBT == null)
 			return;
 		
-		CompoundTag currentCompound = to;
 		
-		// Resolve the path where where we shall copy all nbt data to.
-		for(int i = 0; i < toPath.nodes.length - 1; i++)
-		{
-			CompoundTag newCompound = currentCompound.getCompound(toPath.nodes[i]);
-			currentCompound.put(toPath.nodes[i], newCompound);
-			currentCompound = newCompound;
-		}
-		
-		currentCompound.put(toPath.nodes[toPath.nodes.length - 1], fromNBT.copy());
+		toPath.getOrCreateParent(to).put(toPath.nodes[toPath.nodes.length - 1], fromNBT.copy());
 	}
 	
 	@Override
