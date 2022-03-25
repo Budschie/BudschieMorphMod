a2d6161778744154a54a4a21ed8cfd647158ca34
Budschie <jul.kp06@gmail.com>
2022-03-25 22:55:16 +0100

    NoFlames is now a mere marker ability

--- src/main/java/de/budschie/bmorph/morph/functionality/configurable/NoFlames.java
+++ src/main/java/de/budschie/bmorph/morph/functionality/configurable/NoFlames.java
@@ -3,26 +3,8 @@ package de.budschie.bmorph.morph.functionality.configurable;
 import com.mojang.serialization.Codec;
 
 import de.budschie.bmorph.morph.functionality.Ability;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.TickEvent.Phase;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class NoFlames extends Ability
 {
 	public static final Codec<NoFlames> CODEC = Codec.unit(NoFlames::new);
-	
-	@Override
-	public boolean isAbleToReceiveEvents()
-	{
-		return true;
-	}
-	
-	@SubscribeEvent
-	public void onTick(TickEvent.PlayerTickEvent event)
-	{
-		if(event.phase == Phase.END && isTracked(event.player))
-		{
-			event.player.setRemainingFireTicks(0);
-		}
-	}
 }
