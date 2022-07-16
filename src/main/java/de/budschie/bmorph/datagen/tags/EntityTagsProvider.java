30babaee69d398f16942aeba97328d4d325bfe1b
Budschie <jul.kp06@gmail.com>
2022-07-16 16:57:48 +0200

    Undead mobs will no longer drown

--- src/main/java/de/budschie/bmorph/datagen/tags/EntityTagsProvider.java
+++ src/main/java/de/budschie/bmorph/datagen/tags/EntityTagsProvider.java
@@ -31,5 +31,7 @@ public class EntityTagsProvider extends TagsProvider<EntityType<?>>
 	{
 		this.tag(ModEntityTypeTags.DISABLE_SNEAK_TRANSFORM).add(EntityType.WOLF).add(EntityType.CAT).add(EntityType.PARROT);
 		this.tag(ModEntityTypeTags.IRON_GOLEM_ALIKE).add(EntityType.IRON_GOLEM);
+		this.tag(ModEntityTypeTags.UNDEAD).add(EntityType.ZOGLIN).add(EntityType.PHANTOM).add(EntityType.ZOMBIE).add(EntityType.DROWNED)
+		.add(EntityType.HUSK).add(EntityType.ZOMBIE_VILLAGER).add(EntityType.ZOMBIFIED_PIGLIN).add(EntityType.SKELETON).add(EntityType.STRAY).add(EntityType.WITHER_SKELETON);
 	}
 }
