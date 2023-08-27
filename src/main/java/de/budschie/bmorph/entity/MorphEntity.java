bac6f73f6a5a2cb7b4f4c90fbcd78ad8f65cde76
Budschie <jul.kp06@gmail.com>
2023-08-27 19:32:20 +0200

    Adjusted morph entity to comply with the updated Entity class

--- src/main/java/de/budschie/bmorph/entity/MorphEntity.java
+++ src/main/java/de/budschie/bmorph/entity/MorphEntity.java
@@ -18,6 +18,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -201,7 +202,7 @@ public class MorphEntity extends Entity
 	}
 
 	@Override
-	public Packet<?> getAddEntityPacket()
+	public Packet<ClientGamePacketListener> getAddEntityPacket()
 	{
 		return NetworkHooks.getEntitySpawningPacket(this);
 	}	
