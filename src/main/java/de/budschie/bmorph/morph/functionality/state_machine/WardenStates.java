7189e45c03a66ef7eb6646320baca9371e2f66fd
Budschie <jul.kp06@gmail.com>
2023-12-18 10:16:06 +0100

    Added file containing the state machine keys and values for the warden

---
+++ src/main/java/de/budschie/bmorph/morph/functionality/state_machine/WardenStates.java
@@ -0,0 +1,14 @@
+package de.budschie.bmorph.morph.functionality.state_machine;
+
+import de.budschie.bmorph.main.References;
+import net.minecraft.resources.ResourceLocation;
+
+public class WardenStates
+{
+	public static final ResourceLocation WARDEN_KEY = new ResourceLocation(References.MODID, "warden_state");
+	public static final String EMERGING = "emerge";
+	public static final String DIG = "dig";
+	public static final String SONIC_BOOM = "sonic_boom";
+	public static final String UNDER_GROUND = "under_ground";
+	public static final String DEFAULT = "default";
+}
